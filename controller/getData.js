b7eb98731712c36770106687a2fb0b5792573523
liuqi <boxingliuqi@163.com>
2022-03-17 17:42:22 +0800

    forth

--- controller/getData.js
+++ controller/getData.js
@@ -7,3 +7,27 @@ exports.getNavMenu=(req,res,next)=>{
         res.json(util.getReturnData(0,'',data));
     })
 }
+
+exports.getFooter=(req,res,next)=>{
+    let key=req.headers.fapp+"_footer";
+    redis.get(key).then(data=>{
+        console.log(data);
+        res.json(util.getReturnData(0,'',data));
+    })
+}
+
+exports.getLinks=(req,res,next)=>{
+    let key=req.headers.fapp+"_links";
+    redis.get(key).then(data=>{
+        console.log(data);
+        res.json(util.getReturnData(0,'',data));
+    })
+}
+
+exports.getIndexPic=(req,res,next)=>{
+    let key=req.headers.fapp+"_indexpic";
+    redis.get(key).then(data=>{
+        console.log(data);
+        res.json(util.getReturnData(0,'',data));
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,27 @@ var router = express.Router();
 const util=require('../util/common');
 //引入导航栏逻辑处理
 var {getNavMenu}=require('../controller/getData');
+var {getFooter}=require('../controller/getData');
+var {getLinks}=require('../controller/getData');
+var {getIndexPic}=require('../controller/getData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// test router
-router.get('/getFooter',function(req,res,next){
-  res.json(util.getReturnData(0,'success'));
-});
+// // test router
+// router.get('/getFooter',function(req,res,next){
+//   res.json(util.getReturnData(0,'success'));
+// });
 
 //获取导航栏路由
 router.get('/getNavMenu',getNavMenu);
+//获取底部路由
+router.get('/getFooter',getFooter);
+//获取友情链接路由
+router.get('/getLinks',getLinks);
+//获取首页轮播图路由
+router.get('/getIndexPic',getIndexPic);
 
 module.exports = router;
